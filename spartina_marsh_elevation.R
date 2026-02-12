6db0315d512d146fb50a8b2e4e38dcd1f6607f0b
Katie Emelianova <katieemelianova@Mac.telekom.ip>
2026-02-12 09:09:22 +0100

    useful example of redordering factor for ggplot here

---
+++ spartina_marsh_elevation.R
@@ -0,0 +1,92 @@
+
+
+
+
+samplesheet <- readxl::read_xlsx("/Users/katieemelianova/Desktop/Spartina/JMF_results/Results_2025-04-30/JMF-2503-07.xlsx") %>% dplyr::select(`JMF sample ID`, `User sample ID`)
+sample_annotation <- readxl::read_xlsx("/Users/katieemelianova/Desktop/Spartina/Sequencing/March2025_LeFaouAmplicon/LeFaou_samplesheet.xlsx") %>% dplyr::select(`User sample ID`, `Sample description`)
+sample_mapping <- left_join(samplesheet, sample_annotation, by="User sample ID") %>% rename("JMF_sample_ID" = "JMF sample ID")
+
+
+phylo_elevation <- readRDS("/Users/katieemelianova/Desktop/Spartina/JMF_results/Results_2025-04-30/JMF-2503-07_16S_raw_phyloseq.rds") %>%
+                   tax_filter(min_prevalence = 10,
+                   prev_detection_threshold = 5,
+                   min_total_abundance = 5,
+                   min_sample_abundance = 5) %>%
+  ps_join(sample_mapping, by = "JMF_sample_ID") %>%
+  subset_taxa(!(Family %in% c("Mitochondria", "Chloroplast"))) %>% subset_taxa(!(Order %in% c("Mitochondria", "Chloroplast")))
+
+
+
+phylo_elevation %<>% subset_samples(!(is.na(Sample.description)) & !(Sample.description %in% c("Sedment unknown", "Root unknown")))
+
+
+phylo_elevation_prop <- transform_sample_counts(phylo_elevation, function(otu) otu/sum(otu))
+ord.nmds.bray_elevation <- ordinate(phylo_elevation_prop, method="NMDS", distance="bray")
+
+plot_ordination(phylo_elevation_prop, ord.nmds.bray_elevation, color="Sample.description", title="Bray NMDS") + 
+  geom_point(size = 7) +
+  theme(strip.text.x = element_text(size=25),
+        axis.text.x = element_text(size=25),
+        axis.text.y = element_text(size=20),
+        axis.title = element_text(size=25),
+        legend.title = element_text(size=20),
+        legend.text = element_text(size=20)) +
+  ggtitle("")
+
+
+
+
+
+
+
+
+
+
+
+# geometric means function
+# doing it this way because see here:
+# https://github.com/joey711/phyloseq/issues/445
+gm_mean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
+run_deseq <- function(phylo_object, design_term){
+  formula_parsed<-paste("~", design_term)
+  phylo_deseq <- phylo_object %>% phyloseq_to_deseq2(as.formula(formula_parsed))
+  phylo_deseq_geomeans <- apply(counts(phylo_deseq), 1, gm_mean)
+  phylo_deseq = estimateSizeFactors(phylo_deseq, geoMeans = phylo_deseq_geomeans)
+  phylo_deseq = DESeq(phylo_deseq, fitType="local")
+  return(phylo_deseq)
+}
+
+annotate_deseq_results <- function(deseq_result, phylo_object){
+  annotated <- deseq_result %>% 
+    data.frame() %>% 
+    arrange(padj) %>% 
+    filter(padj < 0.05) %>% 
+    rownames_to_column(var="amplicon") %>% 
+    left_join(tax_table(phylo_object) %>% data.frame() %>% rownames_to_column(var="amplicon"))
+}
+
+
+tst <- subset_samples(phylo_elevation, Sample.description %in% c("Sediment marsh", "Sediment dry")) %>% run_deseq("Sample.description")
+
+resultsNames(tst)
+results(tst)
+
+
+tst_annot <- results(tst) %>% annotate_deseq_results(phylo_elevation)
+
+tst_annot %>% colnames()
+tst_annot %>% 
+  dplyr::select(log2FoldChange, Family) %>% 
+  drop_na() %>%
+  ggplot(aes(x=reorder(Family, log2FoldChange), y=log2FoldChange)) + 
+  geom_bar(stat="identity", color="black", 
+           position=position_dodge())
+
+
+
+
+
+
